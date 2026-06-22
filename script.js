e772cf08c6fd8ca20b3d185256382dd2e36ea3de
tangshuang631 <2429936596@qq.com>
2026-06-22 19:30:20 +0800

    feat: refine landing page copy

--- script.js
+++ script.js
@@ -4,63 +4,21 @@ if (typeof window.gsap !== "undefined" && typeof window.ScrollTrigger !== "undef
   gsap.utils.toArray(".reveal").forEach((element) => {
     const delay = Number(element.dataset.delay || 0);
 
-    gsap.to(element, {
-      opacity: 1,
-      y: 0,
-      duration: 1,
-      delay,
-      ease: "power3.out",
-      scrollTrigger: {
-        trigger: element,
-        start: "top 84%",
-        once: true
+    gsap.fromTo(
+      element,
+      { opacity: 0, y: 12 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        delay,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: element,
+          start: "top 90%",
+          once: true
+        }
       }
-    });
+    );
   });
-
-  if (document.querySelector(".aurora-one")) {
-    gsap.to(".aurora-one", {
-      y: -24,
-      x: 18,
-      duration: 12,
-      repeat: -1,
-      yoyo: true,
-      ease: "sine.inOut"
-    });
-  }
-
-  if (document.querySelector(".aurora-two")) {
-    gsap.to(".aurora-two", {
-      y: 18,
-      x: -22,
-      duration: 14,
-      repeat: -1,
-      yoyo: true,
-      ease: "sine.inOut"
-    });
-  }
-
-  if (document.querySelector(".aurora-three")) {
-    gsap.to(".aurora-three", {
-      y: -16,
-      x: 24,
-      duration: 16,
-      repeat: -1,
-      yoyo: true,
-      ease: "sine.inOut"
-    });
-  }
-
-  if (document.querySelector(".hero") && document.querySelector(".hero-copy")) {
-    gsap.to(".hero-copy", {
-      yPercent: -6,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".hero",
-        start: "top top",
-        end: "bottom top",
-        scrub: 1.1
-      }
-    });
-  }
 }
